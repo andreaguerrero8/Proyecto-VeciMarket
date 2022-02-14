c8c7ff5a550ff6546b0da62a9d388f5a4236157c
Andreina Rodriguez <andreaguerrero.ar@gmail.com>
2022-02-13 20:51:30 -0500

    se sube cambios de pagina de productos con cards realizadas dinamicamente y guarde de informacion en el localStorage

---
+++ scripts/data.js
@@ -0,0 +1,291 @@
+export const productos = [
+       {
+            "id": 1,
+            "nombre": "Aceite",
+            "imagen": "../image/aceite.jpg",
+            "precio": "16.200",
+            "categoria":"cocina"
+
+        },
+        {
+            "id": 2,
+            "nombre": "Agua",
+            "imagen": "../image/agua.png",
+            "precio": "2.000",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 3,
+            "nombre": "Allegra",
+            "imagen": "../image/allegra.jpg",
+            "precio": "43.120",
+            "categoria": "salud"
+
+        },
+        {
+            "id": 4,
+            "nombre": "Antiacido",
+            "imagen": "../image/antiacido.jpg",
+            "precio": "11.650",
+            "categoria": "salud"
+
+        },
+        {
+            "id": 5,
+            "nombre": "Arroz",
+            "imagen": "../image/arroz.jpg",
+            "precio": "3.500",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 6,
+            "nombre": "Azitromicina MK",
+            "imagen": "../image/azitromicina.png",
+            "precio": "11.550",
+            "categoria": "salud"
+
+        },
+        {
+            "id": 7,
+            "nombre": "Azucar",
+            "imagen": "../image/azucar.jpg",
+            "precio": "4.000",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 8,
+            "nombre": "Ferrero Rocher",
+            "imagen": "../image/barraBlanca.jpg",
+            "precio": "5.700",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 9,
+            "nombre": "Crema Ego",
+            "imagen": "../image/ego.jpg",
+            "precio": "8670",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 10,
+            "nombre": "Coca-cola",
+            "imagen": "../image/coca-cola.png",
+            "precio": "2.600",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 11,
+            "nombre": "Doritos",
+            "imagen": "../image/doritos.jpg",
+            "precio": "1.400",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 12,
+            "nombre": "Gatorade",
+            "imagen": "../image/gatorade.jpg",
+            "precio": "3.100",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 13,
+            "nombre": "Harina",
+            "imagen": "../image/harina.png",
+            "precio": "3.900",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 14,
+            "nombre": "Helado",
+            "imagen": "../image/helado.jpg",
+            "precio": "23.520",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 15,
+            "nombre": "Vitamina C",
+            "imagen": "../image/higienicas.jpg",
+            "precio": "8.500",
+            "categoria": "salud"
+
+        },
+        {
+            "id": 16,
+            "nombre": "Huevos",
+            "imagen": "../image/huevos.jpg",
+            "precio": "7.900",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 17,
+            "nombre": "Jabon",
+            "imagen": "../image/jabon.jpg",
+            "precio": "11.400",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 18,
+            "nombre": "Jabon liquido",
+            "imagen": "../image/jabonliquido.jpg",
+            "precio": "10.600",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 19,
+            "nombre": "Jet",
+            "imagen": "../image/jet.png",
+            "precio": "5.270",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 20,
+            "nombre": "Leche",
+            "imagen": "../image/leche.png",
+            "precio": "5.600",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 21,
+            "nombre": "Mani",
+            "imagen": "../image/mani.jpg",
+            "precio": "2.500",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 22,
+            "nombre": "Mantequilla",
+            "imagen": "../image/mantequilla.jpg",
+            "precio": "3.100",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 23,
+            "nombre": "Hershey's",
+            "imagen": "../image/barra.jpg",
+            "precio": "2.400",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 24,
+            "nombre": "Nutella",
+            "imagen": "../image/nutella.png",
+            "precio": "17.600",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 25,
+            "nombre": "Nutribela",
+            "imagen": "../image/nutribela.jpg",
+            "precio": "16.100",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 26,
+            "nombre": "Pan",
+            "imagen": "../image/pan.jpg",
+            "precio": "5.700",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 27,
+            "nombre": "Panela",
+            "imagen": "../image/panela.png",
+            "precio": "4.400",
+            "categoria": "cocina"
+
+        },
+        {
+            "id": 28,
+            "nombre": "Pantene",
+            "imagen": "../image/pantene.png",
+            "precio": "19.800",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 29,
+            "nombre": "Papel Higienico",
+            "imagen": "../image/papel.png",
+            "precio": "21.300",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 30,
+            "nombre": "Ponque",
+            "imagen": "../image/ponque.jpg",
+            "precio": "3.450",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 31,
+            "nombre": "Prestobarbas",
+            "imagen": "../image/prestobarbas.png",
+            "precio": "14.550",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 32,
+            "nombre": "Toallas Stayfree",
+            "imagen": "../image/toalla.jpg",
+            "precio": "10.900",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 33,
+            "nombre": "Todo Rico Bar B-Q",
+            "imagen": "../image/todo-rico.png",
+            "precio": "1.440",
+            "categoria": "snacks"
+
+        },
+        {
+            "id": 34,
+            "nombre": "Unesia",
+            "imagen": "../image/unesia.jpg",
+            "precio": "19.800",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 35,
+            "nombre": "Dolex",
+            "imagen": "../image/dolex.jpg",
+            "precio": "11.300",
+            "categoria": "higiene"
+
+        },
+        {
+            "id": 36,
+            "nombre": "Huggies",
+            "imagen": "../image/huggies.png",
+            "precio": "29.440",
+            "categoria": "higiene"
+
+        }
+
+    ]
---
+++ scripts/script.js
@@ -0,0 +1,90 @@
+import { productos } from "./data.js";
+
+const cards = document.getElementById('cards');
+let datos= [];
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarCards();
+})
+
+
+//muestra las cards de forma dinamica, mediante el forEarch, usando la costante importada productos, 
+//desde el archivo data.js
+function mostrarCards() {
+
+    productos.forEach(element => {
+        const { nombre, imagen, precio, id } = element;
+
+        let fragmento = document.createDocumentFragment();
+        let card = document.createElement('div');
+        card.setAttribute('class', 'box');
+
+
+
+        let imgCard = document.createElement('img');
+        imgCard.setAttribute('src', `${imagen}`)
+
+
+        let contenedorCard = document.createElement('div');
+        contenedorCard.setAttribute('class', 'content');
+
+        let nombreProducto = document.createElement('h3');
+        nombreProducto.textContent = `${nombre}`;
+
+        let precios = document.createElement('div');
+        precios.setAttribute('class', 'precio');
+        precios.textContent = `$ ${precio}`;
+
+        let btnComprar = document.createElement('button');
+        btnComprar.setAttribute('class', 'btn');
+        btnComprar.textContent = 'Comprar';
+        btnComprar.setAttribute('id', `${id}`);
+
+        card.appendChild(imgCard);
+        card.appendChild(contenedorCard);
+
+        contenedorCard.appendChild(nombreProducto);
+        contenedorCard.appendChild(precios);
+        contenedorCard.appendChild(btnComprar);
+
+        fragmento.appendChild(card);
+        cards.appendChild(fragmento);
+    });
+
+}
+
+//escucha el evento click y captura el id del botón, posterior a ello lo envía a la funcion Envio Datos
+
+cards.addEventListener('click', (e) =>{
+    envioDatos(e.target.id);
+})
+
+//se busca el elemento en la data mediante un forEach que contenga el id de la tarjeta oprimida
+// y luego se procede a guardar en un objeto los datos nombre, precio y id, para ser usados mas adelante
+function envioDatos(id) {
+    productos.forEach(element =>{
+        if (element.id == id){
+
+            let algo =
+            {
+            nombre: `${element.nombre}`,
+            precio: `${element.precio}`,
+            id : `${element.id}`
+            }
+
+            // agregar al arreglo datos, el objeto datos que contiene la informacion de la cards seleccionada
+            datos.push(algo); 
+
+            //agregar al localStorage los datos del objeto(datos)
+            localStorage.setItem('Articulos', JSON.stringify(datos));
+
+            //traer datos del localStorage 
+            localStorage.getItem('Articulos');
+
+            console.log(datos);
+        }
+    })
+    
+}
+
